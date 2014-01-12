b10f4b1fb92fceb9e147a3f29b985a2ea32bc0a2
Marin <mgs@jpisoftware.net>
2014-01-11 16:39:54 -0800

    before != after

--- site.go
+++ site.go
@@ -83,7 +83,7 @@ func Index(e mango.Env) (mango.Status, mango.Headers, mango.Body) {
 
 
 	log.Printf("req for %s", f)
-	if t, err := time.Parse(http.TimeFormat, e.Request().Header.Get("If-Modified-Since")); err == nil && t.Before(startTime) {
+	if t, err := time.Parse(http.TimeFormat, e.Request().Header.Get("If-Modified-Since")); err == nil && t.After(startTime) {
 		return 304, expirationHeaders, ""
 	}
 
